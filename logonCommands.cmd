784a2c09184288cb2ae90c5c38e7751ed37ab4d7
JOHN EMBRY <JEmbry@int.chickasaw.net>
2016-12-23 05:50:23 +0000

    updated windows logonCommands.cmd file since last commit

--- logonCommands.cmd
+++ logonCommands.cmd
@@ -2,3 +2,9 @@ DOSKEY ls=dir
 DOSKEY del=rm
 DOSKEY c=cls
 DOSKEY clear=cls
+DOSKEY nrw=npm run webpack
+DOSKEY nrs=npm run start
+DOSKEY jamesroot=cd C:\Users\JEmbry\Developer\Freelancing\Test\JamesHenderson\root
+DOSKEY finder=explorer
+DOSKEY n=start cmd.exe
+cls
