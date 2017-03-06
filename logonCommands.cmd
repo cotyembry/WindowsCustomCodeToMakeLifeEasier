d2e44050211ede232323c418edf0d53dcaf46501
cotyembry <cotyembry@gmail.com>
2017-03-06 13:14:14 -0600

    Update logonCommands.cmd
    added more commands

--- logonCommands.cmd
+++ logonCommands.cmd
@@ -5,5 +5,9 @@ DOSKEY clear=cls
 DOSKEY nrw=npm run webpack
 DOSKEY nrs=npm run start
 DOSKEY jamesroot=cd C:\Users\JEmbry\Developer\Freelancing\Test\JamesHenderson\root
-DOSKEY finder=explorer
+DOSKEY finder=explorer .
 DOSKEY n=start cmd.exe
+DOSKEY sbt="C:\Program Files\Sublime Text 3\sublime_text.exe" $*
+
+DOSKEY pwd=cd
+DOSKEY pbcopy=clip
